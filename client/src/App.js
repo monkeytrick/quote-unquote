73305db86f96377bc54aa7326283073e13e96994
brian stack <brianstack@hotmail.com>
2023-09-06 22:56:25 +0900

    refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,89 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './PeopleOptions';
+import useFetch from './useFetch';
+import QuoteArea from './QuoteArea';
+
 
 function App() {
+  // Change to useState later
+  const names = [];
+
+  //Display quote area
+  const [quoteIsActive, setQuoteIsActive] = useState('false');
+  // Display options
+  const [optionsIsActive, setOptionsIsActive] = useState('false');
+  
+  const {data, isLoading, error} = useFetch('http://localhost:8000/');
+
+  //Handle options 
+  function handler(person) {
+    console.log("click for " + person.name)
+    const max = 2;
+    const names = [];
+
+    if(optionsIsActive == false) {
+      setOptionsIsActive();
+    }  
+  
+    if(names.length < max) {
+      console.log("request for " + person);
+      names.push(person);
+      //create object and push to array
+      //Loop through to create buttons with name as key - include delete option
+      //Or pass array to prop so can loop through to create buttons
+      names.forEach((name) => {})
+    } else {
+      //return error
+      console.log("max names reached");
+    }
+
+ //If 
+    if(names.length = 2) {
+      //Display fetch button or set as prop with values
+    }
+
+
+  }
+  //Fetch button request for quote
+  function quoteRequest () {
+    const url = "http://localhost:8000/";
+    console.log("max num names reached");
+    names.forEach((name) => {})
+  }
+
+  
+
   return (
     <div className="App">
       <Header />
+      <QuoteArea />
 
-      <Home/>
-      {/* Form display fields
-      Name list
-      Quote as modal or field
-      Area to display given quotes */}
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      {/* Pass props to display - set other fields as not active-pass in state and function  */}
+      {/* {quoteIsActive && <QuoteArea />}   */}
+
+        <div>
+
+        { isLoading && <div>Loading...</div> }
+
+        { error && <div>{error}</div> }
+
+        {data && (
+          <div className="row">
+              {data.map((person) => {
+                return <div className="col col-lg-3 m-2">
+                          <button type="button" className="btn btn-outline-success" 
+                                  key={person.name} 
+                                  onClick={handler(person)}>
+                                  {person.name}                            
+                          </button>
+                        </div>
+              })}
+          </div>
+        )}
+       </div> 
 
     </div>
   );
---
+++ client/src/QuoteArea.jsx
@@ -0,0 +1,18 @@
+export default function QuoteArea() {
+
+  return (
+    <div>
+        <div className="row">
+            <div className="quote-area">
+                <h2>Quote Goes Here</h2>
+            </div>
+            <div className="row">
+                <div className="options-area">
+                    Name options go here
+                </div>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
